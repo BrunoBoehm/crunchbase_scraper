5e69b8c9cc5f46c61495ea505e805c2875d0a46e
BrunoBoehm <bruno.boehm@gmail.com>
2017-01-23 16:06:47 +0100

    added nokogiri to gemspec, and pry for dev env

--- lib/crunchbase_scraper.rb
+++ lib/crunchbase_scraper.rb
@@ -1,4 +1,8 @@
 # this acts as our config.rb file
+require 'nokogiri'
+require 'pry'
+#we'll have to take out the pry require statement when publishing since we only want that dependency for development.
+
 
 require_relative "./crunchbase_scraper/version"
 require_relative "./crunchbase_scraper/cli"
